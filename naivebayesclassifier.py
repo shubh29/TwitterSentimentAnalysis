aa804d9460d50477f97414a6a81bebdedb2eb002
Shubham Mahajan <shubham.mahajan@icloud.com>
2015-11-30 16:10:45 -0500

    Added some comments + fixed minor issues

---
+++ naivebayesclassifier.py
@@ -0,0 +1,79 @@
+'''
+Implementation of Naive Bayes Classifier for tweets
+'''
+import sys
+import math
+from classifier import Classifier
+
+class NaiveBayesClassifier(Classifier):
+    def __init__(self, fname, *args, **kargs):
+        Classifier.__init__(self, fname, *args, **kargs)
+
+        # sometimes a threshold value is trained during Bayesian
+        # classification to avoid classifying too many 'documents' as
+        # one kind or the other
+        self.thresholds = [1.0, 1.0]
+
+    def setThresholds(self, neg=1.0, pos=1.0):
+        self.thresholds = [neg, pos]
+
+    def probTweetClass(self, text, c):
+        '''
+        Returns the (log) probability of a tweet, given a particular class
+        P(tweet | class)
+        '''
+        features = self.getFeatures(text)
+        p = 0
+        for f in features:
+            p += math.log(self.weightedProb(f, c))
+        return p
+
+    def probClassTweet(self, text, c):
+        '''
+        Returns the (log) probability of a class, given a particular tweet
+        P(class | tweet) = P(tweet | class) x P(class) / P(tweet)
+        But P(tweet) is constant for all classes; so forget
+        '''
+        return self.probTweetClass(text, c) + math.log(self.probC(c))
+
+    def classify(self, text):
+        '''
+        Returns 0 (negative) if P(class=0 | tweet) > P(class=1 | tweet) * thresholds[0]
+        Return 1 (positive) if P(class=1 | tweet) > P(class=0 | tweet) * thresholds[1]
+        Else return -1 (neutral)
+        '''
+        p0 = self.probClassTweet(text, 0)
+        p1 = self.probClassTweet(text, 1)
+
+        if p0 > p1 + math.log(self.thresholds[0]):
+            return 0
+        elif p1 > p0 + math.log(self.thresholds[1]):
+            return 1
+        else:
+            return -1
+
+    def __repr__(self):
+        return "Classifier info: (weight=%s, grams=%s, thresholds=%s)" % (self.weight, self.numgrams, self.thresholds)
+
+
+def main():    
+    # file to get training data from
+    fromf = 'trainingandtestdata/training.csv'
+    naive = NaiveBayesClassifier(fromf)
+    naive.trainClassifier()
+
+    # optionally, pass in some tweet text to classify
+    if len(sys.argv) == 2:
+        print
+        text = sys.argv[1]
+        result = naive.classify(text)
+        if result == 0:
+            print("'%s' predicted to be Negative" % text)
+        elif result == 1:
+            print("'%s' predicted to be Positive" % text)
+        else:
+            print("'%s' predicted to be Neutral" % text)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ naivebayesevaluator.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+'''
+Contains implementation of Naive Bayes Classifier based upon
+Generic Classifier for tweets.
+
+Options:
+--csvout -> put results in a CSV file
+--stdout -> print results to STDOUT
+
+Usage:
+python naivebayesevaluator.py [(--csvout | --stdout) --dev -g <grams-list> 
+                              -w <weights-start, weights-end, step>
+                              -t <thresholds-start, thresholds-end, step>]
+
+For example, 
+python naivebayesevaluator.py --csvout --dev -g 1 1,2 -w 0.1,1.5,0.5 -t 1.0,2.0,0.5
+
+should evaluate naive bayes (on the DEV set) using:
+1. unigrams and unigrams + bigrams. 
+2. weights 0.1, 0.6, 1.1
+3. threshold values 1.0, 1.5, 2.0
+
+and store the result in the file 'stats/nbevaluatorstats<current datetime>.csv'.
+python naivebayesevaluator.py -g 1 1,2 should print out accuracy of 
+naive bayes on the TRAINING set.
+'''
+from evaluator import Evaluator
+from naivebayesclassifier import NaiveBayesClassifier
+import csv
+import datetime
+import sys
+import argparse
+
+class NaiveBayesEvaluator(Evaluator):
+    def __init__(self, trainfile, devfile, testfile, *args, **kargs):
+        Evaluator.__init__(self, trainfile, devfile, testfile, *args, **kargs)
+
+        self.allthresholds = kargs.get("allthresholds")
+        self.csvout = kargs.get("csvout", False)
+        self.results = []
+
+    def flushToCSV(self):
+        fname = "stats/nbevaluatorstats%s.csv" % \
+                str(datetime.datetime.now()).replace(' ', '-')
+        with open(fname, "wb") as f:
+            w = csv.writer(f, delimiter=',', quotechar='"')
+            # write out header            
+            w.writerow(["Classifier Info",
+                        "Accuracy for Positives (%)",
+                        "Accuracy for Negatives (%)",
+                        "Accuracy for (Positives|Negatives) (%)",
+                        "Correlation for (Positives|Negatives) (%)"])
+            for row in self.results:
+                w.writerow(row)
+        print("Flushing results of Naive Bayes evaluation into '%s'..." % fname)
+
+    def run(self):
+        if not self.usedev:
+            for grams in self.allgrams:
+                c = NaiveBayesClassifier(self.rawfname,
+                                         grams=grams)
+                c.trainClassifier()
+                self.stdout = True
+                self.evaluate(c)
+            return
+            
+        for grams in self.allgrams:
+            c = NaiveBayesClassifier(self.rawfname,
+                                     grams=grams)
+            c.trainClassifier()
+            
+            for w in self.allweights:
+                c.setWeight(w)                                
+        
+                for t1 in self.allthresholds:
+                    for t2 in self.allthresholds:
+                        c.setThresholds(neg=t1, pos=t2)
+                        cinfo, accpos, accneg, accall, corrall = self.evaluate(c)
+                        self.results.append([cinfo, accpos, accneg,
+                                             accall, corrall])
+
+        if self.csvout:
+            self.flushToCSV()
+
+def processGrams(glist):
+    return [[int(eachr) for eachr in each.split(',')] for each in glist]
+
+def floatrange(start, end, step):
+    return [start + step*x for x in range(int((end-start)/step)+1)]
+
+def processWT(wstr):
+    start, end, step = [float(res) for res in wstr.split(',')]
+    return floatrange(start, end, step)
+
+def main():
+    trainfile = "trainingandtestdata/training.csv"
+    devfile = "trainingandtestdata/devset.csv"
+    testfile = "trainingandtestdata/testing.csv"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--csvout", dest="csvout",
+                        action="store_true", default=False)
+    parser.add_argument("--stdout", dest="stdout", 
+                        action="store_true", default=False)
+    parser.add_argument("--dev", dest="dev",
+                        action="store_true", default=False)
+    parser.add_argument("-g", dest="g", nargs="+",
+                        metavar="x,y,z,..", required=True)
+    parser.add_argument("-w", dest="w",
+                        metavar="START, END, STEP", required=False)
+    parser.add_argument("-t", dest="t",
+                        metavar="START, END, STEP", required=False)
+
+    args = parser.parse_args()
+    grams = processGrams(args.g)
+
+    try:
+        if args.g and args.w and args.t:
+            weights = processWT(args.w)
+            thresholds = processWT(args.t)
+        else:
+            weights = thresholds = []
+            args.dev = False
+            
+        nbEvaluator = NaiveBayesEvaluator(trainfile, devfile, testfile,
+                                          allgrams=grams,
+                                          allweights=weights,
+                                          allthresholds=thresholds,
+                                          csvout=args.csvout,
+                                          stdout=args.stdout,
+                                          usedev=args.dev)        
+        nbEvaluator.run()
+    
+    except: 
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
